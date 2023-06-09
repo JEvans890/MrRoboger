e77655852a41d20e8270a67184807b399ae66a44
JEvans890 <evansjexp@gmail.com>
2023-06-09 16:06:48 -0700

    add changes to variables and more specific references to HTML and attempts to return a value to UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,33 @@
-window.onload = function();
-let form = document.querySelector("form");
-form.onsubmit = function(event) {
-    const userInput = document.querySelector("userinput");
-    const textArray = text.split(" ");
-    const userText = ["1", "2", "3"];
-    const Beep = document.innerText("Beep!");
-    const Boop = document.innerText("Boop!");
-    const Neighbor = document.innerText("Won't you be my neighbor?");
-    let borogerOutput = ["Beep!", "Boop!", "Won't you be my neighbor?"];
-        let Replacement() {
-        if (userInput.includes.innerText("1")) {
-            userInput.replaceWith(Beep);
+window.onload = function() {
+    let form = document.querySelector("form");
+    form.onsubmit = function(event) {
+      event.preventDefault(); // Prevent form submission
+  
+      const userInput = document.querySelector("#userinput").value; // Use # to select element by id and retrieve its value
+      const textArray = userInput.split(" "); // Split the user input into an array of words
+      const userText = ["1", "2", "3"];
+      const Beep = "Beep!"; // Store the strings directly without using innerText
+      const Boop = "Boop!";
+      const Neighbor = "Won't you be my neighbor?";
+      let borogerOutput = ["Beep!", "Boop!", "Won't you be my neighbor?"];
+  
+      // Define a function instead of using let and parentheses
+      function Replacement() {
+        if (userText.includes("1")) {
+          document.querySelector("p").textContent = Beep; // Use textContent to set the paragraph's text
         }
-        if (userInput.includes.innerText("2")) {
-            userInput.replaceWith(Boop);
+        if (userText.includes("2")) {
+          document.querySelector("p").textContent = Boop;
         }
-        if (userInput.includes.innerText("3")) {
-            userInput.replaceWith(Neighbor);
+        if (userText.includes("3")) {
+          document.querySelector("p").textContent = Neighbor;
         }
-        };
-    return Replacement;
-    document.querySelector("p").removeAttribute("hidden");
-    event.preventDefault();
-}
+      }
+  
+      document.querySelector("p").removeAttribute("hidden"); // Remove the "hidden" attribute from the paragraph
+  
+      Replacement(); // Call the Replacement function to perform the replacements
+  
+      return false; // Returning false prevents the form from being submitted
+    };
+  };
